ed7dc6b852386bf9963a1602b5ffd74bd25d67f3
Jon <jbrundagejr@gmail.com>
2021-03-20 13:56:39 -0400

    commit tree 0ae48bbe11505c90c21108f68224a35c6c3661df

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function findMatching(drivers, name) {
-    let foundDriver = drivers.filter(function(driver) {
+    drivers.filter(function(driver) {
         return driver.toLowerCase() === name.toLowerCase()
     })
-    return foundDriver
 }
+
 function fuzzyMatch(drivers, ab) {
     let fuzzyDriver = drivers.filter(function(driver){
-        let firstTwo = driver.substring(0, 2)
-        if (firstTwo === ab)
+        if (driver.substring(0, 2) === ab)
             return driver
     })
     return fuzzyDriver
 }
+
 function matchName(drivers, name) {
     let matchedName = drivers.filter(function(driver) {
         if (driver.name === name) {
